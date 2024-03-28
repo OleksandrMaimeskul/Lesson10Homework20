d98844d39d225758bf6710432122b4d65c72b1a0
OleksandrMaimeskul <maymeskulsasha@gmail.com>
2024-03-28 18:12:55 +0200

    Lesson10Homework20 1st Batch

---
+++ src/Program.java
@@ -0,0 +1,6 @@
+public class Program {
+    public static void main(String[] args) {
+        User alice = new User("Alice", "qwerty");
+        alice.displayAccount();
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,27 @@
+public class User {
+    private String login;
+    Account account;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.account = new Account(password);
+    }
+
+    class Account {
+        private String password;
+
+        public Account(String password) {
+            this.password = password;
+        }
+
+        public void displayAccount() {
+            System.out.printf("Account Login successful! Login: %s, Password: %s ", login, password);
+        }
+
+    }
+    public void displayAccount() {
+        account.displayAccount();
+    }
+}
+
+
